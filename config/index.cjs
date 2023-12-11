83fb5847df849a7a90f9096038f5b1ebe846cd3b
firefly <45292131+wxdbb@users.noreply.github.com>
2023-12-11 19:04:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -41,7 +41,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2016-04-08' },
+        { keyword: 'love_day', date: '2016-04-12' },
         // 结婚纪念日
         //{ keyword: 'marry_day', date: '2022-09-09' },
       ],
