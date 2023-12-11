c80454dcb3487bc07be346a53271ab36f0163ed4
firefly <45292131+wxdbb@users.noreply.github.com>
2023-12-11 18:53:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '吴瑕',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU27115TsydvkGRHC1KiwD7RATfzhD5q4xvxmLor',
+      id: 'PDU27119TGTBF9z2RxtKrfIfSw7KOYKIQ0kwlyF2n',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
