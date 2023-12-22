4004acb071e7e4b59766801d78740cdd15f5c24d
firefly <45292131+wxdbb@users.noreply.github.com>
2023-12-22 21:51:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,13 +23,13 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU27119TGTBF9z2RxtKrfIfSw7KOYKIQ0kwlyF2n',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: '0003',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-17',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '2000', date: '03-17',
+          type: '*生日', name: '宝贝', year: '2000', date: '02-12',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         /*
