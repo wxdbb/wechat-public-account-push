68bf28751d44c2e791d333c6f0473140ae4af27b
firefly <45292131+wxdbb@users.noreply.github.com>
2023-12-22 21:50:01 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -52,6 +52,41 @@ const TEMPLATE_CONFIG = [
       失败用户: {{fail_post_ids.DATA}}
     `
   },
+  {
+    id: '0003',
+    title: '亲爱的, 早上好',
+    desc: `
+      **{{date.DATA}}**
+      
+      ---
+      
+      城市：{{city.DATA}}
+      
+      天气：{{weather.DATA}}
+      
+      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
+      
+      空气质量：{{aqi.DATA}}
+      {{notice.DATA}}
+      金山每日一句：{{note_ch.DATA}}
+      每日一言：{{one_talk.DATA}}
+      土味情话：{{earthy_love_words.DATA}}
+      朋友圈文案:{{moment_copyrighting.DATA}}
+      毒鸡汤：{{poison_chicken_soup.DATA}}
+      
+      ---
+      
+      今天是我们相识的第{{love_day.DATA}}天
+      
+      {{birthday_message.DATA}}
+      
+      ---
+      
+      {{moment_copyrighting.DATA}}
+      
+      {{poetry_title.DATA}} {{poetry_content.DATA}}
+    `
+  },
 ]
 
 module.exports = TEMPLATE_CONFIG
